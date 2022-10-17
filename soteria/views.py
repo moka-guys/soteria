a374d76fce74232dbdd6d90714c301a15defafaf
manifesto <13493869+manifestoso@users.noreply.github.com>
2022-10-18 10:26:13 +1100

    NSWHP-Genomics/ToDos-DevOps#71 - demo init

--- soteria/views.py
+++ soteria/views.py
@@ -1,8 +1,9 @@
 import imghdr
 import os
+import subprocess
 from flask import Flask, render_template, request, redirect, url_for, abort
 from werkzeug.utils import secure_filename
-from . import app
+from . import app 
 
 def validate_image(stream):
     """Your validation logic should go into a similar validation function"""
@@ -27,7 +28,33 @@ def upload_files():
     if filename != '':
         file_ext = os.path.splitext(filename)[1]
         valid_extension = file_ext in app.config['UPLOAD_EXTENSIONS']
-        if not valid_extension or file_ext != validate_image(uploaded_file.stream):
-            abort(400)
+        # if not valid_extension or file_ext != validate_image(uploaded_file.stream):
+        #     abort(400)
         uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
+        ## NSWHP demo
+        validation_command = 'python3 .lint/validator/validate.py --samplesheet uploads/{0}'.format(filename)
+        # try:
+        #     validation_results = subprocess.run (
+        #         [validation_command], shell=True
+        #     )
+        try:
+            validation_results = subprocess.check_output (
+                [validation_command], shell=True
+            )
+        except subprocess.CalledProcessError as e:
+            return "An error occurred while trying to fetch task status updates."
+            if e.output.startswith('error: {'):
+                error = json.loads(e.output[7:]) # Skip "error: "
+                print(error['code'])
+                print(error['message'])
+        return 'Samplesheet Validation: %s' %(validation_results)
     return redirect(url_for('index'))
+
+@app.route('/validator', methods=['POST'])
+def csv_validate():
+    print('========')
+
+    uploaded_file = request.files['file']
+    filename = secure_filename(uploaded_file.filename)
+    print(filename)
+    
